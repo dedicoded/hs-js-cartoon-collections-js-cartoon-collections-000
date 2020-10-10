99971b71f1761c711f5ae55eac549ebe8fdf8d14
Louis Joe <newhopemissionliberia@gmail.com>
2020-10-10 01:19:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 function dwarfRollCall(dwarves) {
-}
-
-function summonCaptainPlanet(planeteerCalls){
-}
-
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
-}
-["Doc", "Dopey", "Bashful", "Grumpy"]
-
-function dwarfRollCall(dwarves) {
   let listDwarves = []
   for(let i = 0; i < dwarves.length; i++){
     
