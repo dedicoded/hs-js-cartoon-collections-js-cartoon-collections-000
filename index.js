d6301f868a45aabaed0a9c2b5937d857bcc1682f
Louis Joe <newhopemissionliberia@gmail.com>
2020-10-15 11:51:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//["Doc", "Dopey", "Bashful", "Grumpy"]
 function dwarfRollCall(dwarves) {
   let listDwarves = []
   for(let i = 0; i < dwarves.length; i++){
@@ -5,10 +6,13 @@ function dwarfRollCall(dwarves) {
     listDwarves.push(`${i+1}. ${dwarves[i]} `)
     
   }
-  return listDwarves.join(""); //The join() method joins all elements of an array (or an array-like object) into a string and returns this string.
+  return listDwarves.join(""); 
+  
+  //The join() method joins all elements of an array (or an array-like object) into a string and returns this string.
 }
+// "1. Doc 2. Dopey 3. Bashful 4. Grumpy "
 
-
+//["earth", "wind", "fire", "water", "heart"]
 function summonCaptainPlanet(planeteerCalls){
   let uppercasePlaneteerCalls = [];
   for(let i = 0; i < planeteerCalls.length; i++){
@@ -17,7 +21,7 @@ function summonCaptainPlanet(planeteerCalls){
   }
   return uppercasePlaneteerCalls
 }
-
+//["EARTH!", "WIND!", "FIRE!", "WATER!", "HEART!"]
 
 
 function longPlaneteerCalls(words) {
@@ -30,7 +34,13 @@ function longPlaneteerCalls(words) {
   }
   return false
 }
+//shortWords = ["wind", "fire"]
+longPlaneteerCalls(short_words)
+//false
 
+assortedWords = ["earth", "wind", "heart", "fire"]
+longPlaneteerCalls(assorted_words)
+//#=> true
 
 
 function findTheCheese (foods) {
@@ -43,3 +53,15 @@ function findTheCheese (foods) {
   }
   return 'no cheese!'
 }
+//snacks = ["crackers", "gouda", "thyme"]
+findTheCheese(snacks)
+//#=> "gouda"
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+findTheCheese(soup)
+//#=> "cheddar"
+
+//Sadly,
+ingredients = ["garlic", "rosemary", "bread"]
+findTheCheese(ingredients)
+//#=> null
