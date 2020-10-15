9a2c772343cad8111e1db4384e7c8bc0ad5f0e60
Louis Joe <newhopemissionliberia@gmail.com>
2020-10-15 14:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function dwarfRollCall(dwarves) {
   }
   return listDwarves.join(""); 
   
-  //The join() method joins all elements of an array (or an array-like object) into a string and returns this string.
-}
+  //The join() method joins all elements of an array (or an array-like object) to string then it returns this string.
+
 // "1. Doc 2. Dopey 3. Bashful 4. Grumpy "
 
 //["earth", "wind", "fire", "water", "heart"]
@@ -23,7 +23,8 @@ function summonCaptainPlanet(planeteerCalls){
 }
 //["EARTH!", "WIND!", "FIRE!", "WATER!", "HEART!"]
 
-
+//shortWords = ["wind", "fire"]
+//assortedWords = ["earth", "wind", "heart", "fire"]
 function longPlaneteerCalls(words) {
   
   for(let i = 0; i < words.length; i++){
@@ -47,21 +48,21 @@ function findTheCheese (foods) {
   
   for (var i = 0; i < foods.length; i++) {
     
-    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
+    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert'|| foods[i]=== 'mozarella' ) {
       return foods[i];
     }
   }
   return 'no cheese!'
 }
-//snacks = ["crackers", "gouda", "thyme"]
-findTheCheese(snacks)
-//#=> "gouda"
-
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-findTheCheese(soup)
-//#=> "cheddar"
-
-//Sadly,
-ingredients = ["garlic", "rosemary", "bread"]
-findTheCheese(ingredients)
-//#=> null
+//BOB, Carol, Diane
+function startsWithB(names)
+ let listNames = []
+ listNames.push(`${i+1}. ${names[i]} `)
+ for (var i = 0; i < names.lenght; i++) {
+    if (names[i].charAt(0) === 'b') 
+    return names[i];
+  
+  
+   
+  
+  }
